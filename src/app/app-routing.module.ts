491408fd19b5b8441cbd020d3b73fff04f191049
itaitã Araujo <itaitan2@gmail.com>
2022-04-15 22:59:35 -0300

    Rotas do crud cliente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,7 @@
+import { ProdutoDeleteComponent } from './components/produto/produto-delete/produto-delete.component';
+import { ProdutoUpdateComponent } from './components/produto/produto-update/produto-update.component';
+import { ProdutoCreateComponent } from './components/produto/produto-create/produto-create.component';
+import { ProdutoListComponent } from './components/produto/produto-list/produto-list.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 import { NgModule } from '@angular/core';
@@ -6,7 +10,11 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [{
   path: '', component: NavComponent, children: [{
     path: 'home', component: HomeComponent
-  }]
+  },
+  { path: 'produtos', component: ProdutoListComponent },
+  { path: 'produtos/create', component: ProdutoCreateComponent },
+  { path: 'produtos/update/:id', component: ProdutoUpdateComponent },
+  { path: 'produtos/delete/:id', component: ProdutoDeleteComponent },]
 }];
 
 @NgModule({
