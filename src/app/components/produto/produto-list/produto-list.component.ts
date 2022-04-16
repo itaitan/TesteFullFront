046c8d0b0ca662126067b8056fd0e67a595e59e4
itaitã Araujo <itaitan2@gmail.com>
2022-04-15 23:11:24 -0300

    List Produto

--- src/app/components/produto/produto-list/produto-list.component.ts
+++ src/app/components/produto/produto-list/produto-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Produto } from './../../../models/produto';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ProdutoService } from 'src/app/services/produto.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-produto-list',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produto-list.component.scss']
 })
 export class ProdutoListComponent implements OnInit {
+  ELEMENT_DATA: Produto[] = [];
 
-  constructor() { }
+  displayedColumns: string[] = [
+    'id',
+    'nome',
+    'preco',
+    'quantidade',
+    'descricao',
+    'acoes',
+  ];
+  dataSource = new MatTableDataSource<Produto>(this.ELEMENT_DATA);
+
+  constructor(private service: ProdutoService) {}
 
   ngOnInit(): void {
+    this.findAll();
   }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  findAll() {
+    this.service.findAll().subscribe((resposta) => {
+
+      this.ELEMENT_DATA = resposta;
+      this.dataSource = new MatTableDataSource<Produto>(resposta);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
 }
