ae3fdb6880a0f497835735da1d67cf5b7c828dca
itaitã Araujo <itaitan2@gmail.com>
2022-04-15 22:52:54 -0300

    Começando projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', component: NavComponent, children: [{
+    path: 'home', component: HomeComponent
+  }]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
