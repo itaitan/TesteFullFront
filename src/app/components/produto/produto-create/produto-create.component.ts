f57b87fec65e78191794a7a7dce4e5afd8d756a6
itaitã Araujo <itaitan2@gmail.com>
2022-04-15 23:37:58 -0300

    create

--- src/app/components/produto/produto-create/produto-create.component.ts
+++ src/app/components/produto/produto-create/produto-create.component.ts
@@ -1,15 +1,60 @@
+import { Produto } from './../../../models/produto';
+import { ProdutoService } from './../../../services/produto.service';
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-produto-create',
   templateUrl: './produto-create.component.html',
-  styleUrls: ['./produto-create.component.scss']
+  styleUrls: ['./produto-create.component.scss'],
 })
 export class ProdutoCreateComponent implements OnInit {
+  produto: Produto = {
+    nome: '',
+    preco: 0,
+    quantidade: 0,
+    descricao: '',
+  };
 
-  constructor() { }
+  nome: FormControl = new FormControl();
+  preco: FormControl = new FormControl();
+  quantidade: FormControl = new FormControl();
+  descricao: FormControl = new FormControl();
 
-  ngOnInit(): void {
+  constructor(
+    private service: ProdutoService,
+    private tost: ToastrService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+
+  create(): void {
+    this.service.create(this.produto).subscribe(
+      (resposta) => {
+        this.tost.success('Produto cadastrado com sucesso.', 'Cadastro');
+        this.router.navigate(['produto']);
+      },
+      (ex) => {
+        if (ex.error.errors) {
+          ex.error.errors.forEach((element) => {
+            this.tost.error(element.message);
+          });
+        } else {
+          this.tost.error(ex.error.message);
+        }
+      }
+    );
   }
 
+  validaCampos(): boolean {
+    return (
+      this.nome.valid &&
+      this.preco.valid &&
+      this.quantidade.valid &&
+      this.descricao.valid
+    );
+  }
 }
